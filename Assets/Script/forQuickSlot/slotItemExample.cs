cfac2f364c757dfd9a1e2ec91462971bff5498b3
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-30 22:22:38 +0900

    2020.11.30 버프시스템 구현

--- Assets/Script/forQuickSlot/slotItemExample.cs
+++ Assets/Script/forQuickSlot/slotItemExample.cs
@@ -1,28 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class slotItemExample : MonoBehaviour
 {
-    public float buffTime = 2f;
+    public float buffTime = 10f;    //버프가 지속되는 시간
+    float curTime;
+
+
+    public Image icon;
+    void Start(){
+
+    }
+    void Awake(){
+        icon = GetComponent<Image>();
+        icon.fillAmount = 1;
+        curTime = buffTime;
+    }
     void Update()
     {
         if(Input.inputString == (transform.parent.GetComponent<Slot>().num + 1).ToString()){
             // use item
             Debug.Log("You used item");
-            StartCoroutine(ButffSystem());
+            playerAttack.coolTime = playerAttack.coolTime/2.0f;
+            StartCoroutine(BuffSystem());
         }
     }
-
-    IEnumerator ButffSystem() {
-        Debug.Log("공속 증가");
-        playerAttack.coolTime = playerAttack.coolTime/2.0f;
-
-        yield return new WaitForSeconds(buffTime);
-        Debug.Log("다시 원래대로");
+    WaitForSeconds waitTime = new WaitForSeconds(0.1f);
+    IEnumerator BuffSystem() {
+        while (curTime > 0) {
+            curTime -= 0.1f;
+            icon.fillAmount = curTime/buffTime;
+            yield return waitTime;
+        }
+        Debug.Log("버프 끝");
         playerAttack.coolTime = playerAttack.coolTime*2.0f;
-        Destroy(this.gameObject);
+        Destroy(this.gameObject);    
+        
     }
+
     
-    
+
 }
