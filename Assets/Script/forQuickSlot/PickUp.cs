55387b6b6e36603f5fa4200f8f783b752b1a383e
husk <68003176+JangHanjun@users.noreply.github.com>
2020-12-03 21:33:25 +0900

    2020.12.03 백업함
    함부로 유니티 건드는게 아니다

--- Assets/Script/forQuickSlot/PickUp.cs
+++ Assets/Script/forQuickSlot/PickUp.cs
@@ -9,7 +9,7 @@ public class PickUp : MonoBehaviour
     // 하이어라키 > 이미지 를 통해 만든 아이템의 이미지를 slotItem에 넣어주면 됨
     public GameObject slotItem;     // 생성할 아이템의 ui 프리팹
     void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag.Equals("Player")){ //플레이어가 닫는 순간 실행
+        if(collision.gameObject.tag == "Player"){ //플레이어가 닫는 순간 실행
             Inventory inven = collision.GetComponent<Inventory>();
             for(int i = 0; i < inven.slots.Count; i++){        // 인벤토리 슬롯을
                 if(inven.slots[i].isEmpty){                    // 처음부터 검사해서 비어있는지 확인
